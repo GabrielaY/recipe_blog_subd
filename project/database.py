0ea7904917f1b5411c9a8adfd5b52ff34a8c4c64
Ognian Baruh <ognian@baruh.net>
2020-06-09 21:32:38 +0300

    Added pipenv

---
+++ project/database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+DB_NAME = 'database.db'
+
+conn = sqlite3.connect(DB_NAME)
+
+conn.cursor().execute('''
+
+	CREATE TABLE IF NOT EXISTS users (
+		Id INTEGER PRIMARY KEY AUTOINCREMENT,
+		Username TEXT NOT NULL,
+		Password TEXT NOT NULL,
+		Email TEXT NOT NULL
+	)
+
+''')
+
+conn.commit()
+
+class DB:
+	def __enter__(self):
+		self.conn = sqlite3.connect(DB_NAME)
+		return self.conn.cursor()
+
+	def __exit__(self, type, value, traceback)
+		self.conn.commit()
